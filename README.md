13795017ed0ab8d781a3627ef083add41f99aa61
Sevgin SERBEST <sevgin.ss@gmail.com>
2021-10-16 13:34:53 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ters Üçgen
+
+### Java ile basamak sayısının kullanıcıdan alınan ve döngüler kullanılarak, yıldızlar(*) ile ekrana ters üçgen çizen programı
---
+++ src/TersUcgen.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("A number : ");
+        int n = input.nextInt();
+
+        for (int i = (n-1); i >= 0 ; i--) {
+            for (int j = (n-i); j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = (2 * i); k >= 0; k--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
